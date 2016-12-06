59284112f1ab589165a89080ae81f09d6dafda47
amjedonline <amjedonline@gmail.com>
2016-12-06 11:12:06 +0100

    Added new checks in the test case.

--- src/test/java/com/goeuro/busroutes/BusroutesRestServiceIT.java
+++ src/test/java/com/goeuro/busroutes/BusroutesRestServiceIT.java
@@ -23,6 +23,8 @@ public class BusroutesRestServiceIT {
 
 	private RestTemplate restTemplate;
 
+	private static final String ENDPOINT = "http://localhost:8080/api/direct";
+
 	public BusroutesRestServiceIT() {
 
 		restTemplate = new RestTemplate();
@@ -40,30 +42,36 @@ public class BusroutesRestServiceIT {
 
 	@Test
 	public void testDirectRouteNotAvailable() {
-		final ResponseEntity<BusrouteResponse> entity = restTemplate
-				.getForEntity("http://localhost:8080/api/direct?dep_sid=3&arr_sid=6", BusrouteResponse.class);
+		final ResponseEntity<BusrouteResponse> entity = restTemplate.getForEntity(ENDPOINT + "?dep_sid=3&arr_sid=6",
+				BusrouteResponse.class);
 
 		Assert.assertTrue(entity.getStatusCode().is2xxSuccessful());
 
 		final BusrouteResponse response = entity.getBody();
+
+		Assert.assertTrue("3".equals(response.getSource()));
+		Assert.assertTrue("6".equals(response.getDestination()));
 		Assert.assertFalse(response.isDirectBusRoute());
 	}
 
 	@Test
 	public void testDirectRouteAvailable() {
-		final ResponseEntity<BusrouteResponse> entity = restTemplate
-				.getForEntity("http://localhost:8080/api/direct?dep_sid=153&arr_sid=12", BusrouteResponse.class);
+		final ResponseEntity<BusrouteResponse> entity = restTemplate.getForEntity(ENDPOINT + "?dep_sid=153&arr_sid=12",
+				BusrouteResponse.class);
 
 		Assert.assertTrue(entity.getStatusCode().is2xxSuccessful());
 
 		final BusrouteResponse response = entity.getBody();
+
+		Assert.assertTrue("153".equals(response.getSource()));
+		Assert.assertTrue("12".equals(response.getDestination()));
 		Assert.assertTrue(response.isDirectBusRoute());
 	}
 
 	@Test
 	public void testForSourceNotGiven() {
-		final ResponseEntity<BusrouteResponse> entity = restTemplate
-				.getForEntity("http://localhost:8080/api/direct?dep_sid=&arr_sid=12", BusrouteResponse.class);
+		final ResponseEntity<BusrouteResponse> entity = restTemplate.getForEntity(ENDPOINT + "?dep_sid=&arr_sid=12",
+				BusrouteResponse.class);
 
 		Assert.assertTrue(entity.getStatusCode().is4xxClientError());
 
@@ -71,16 +79,16 @@ public class BusroutesRestServiceIT {
 
 	@Test
 	public void testForDestinationNotGiven() {
-		final ResponseEntity<BusrouteResponse> entity = restTemplate
-				.getForEntity("http://localhost:8080/api/direct?dep_sid=153&arr_sid=", BusrouteResponse.class);
+		final ResponseEntity<BusrouteResponse> entity = restTemplate.getForEntity(ENDPOINT + "?dep_sid=153&arr_sid=",
+				BusrouteResponse.class);
 
 		Assert.assertTrue(entity.getStatusCode().is4xxClientError());
 	}
 
 	@Test
 	public void testForParamsAbsent() {
-		final ResponseEntity<BusrouteResponse> entity = restTemplate
-				.getForEntity("http://localhost:8080/api/direct?dep_sid=&arr_sid=", BusrouteResponse.class);
+		final ResponseEntity<BusrouteResponse> entity = restTemplate.getForEntity(ENDPOINT + "?dep_sid=&arr_sid=",
+				BusrouteResponse.class);
 
 		Assert.assertTrue(entity.getStatusCode().is4xxClientError());
 
